a65e3b3187264320b9952cf4d6ee68c9bcc2ec3e
Jenny C <jennyhc888@gmail.com>
2024-03-10 20:32:25 -0700

    Complete Lesson_1_3

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
+import { useState } from "react";
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
+import "./App.css";
 
+const todoList = [
+    { id: 1, title: "Complete lesson one assignment" },
+    { id: 2, title: "Practice Leetcode" },
+    { id: 3, title: "Do Laundry" },
+];
 
 function App() {
-  return (
-    <>
-      <div>
-      <h1>
-        Todo List
-      </h1>
-      <TodoList/>
-      <AddTodoForm/>
-      </div>  
-    </>
-  )
+    const [newTodo, setNewTodo] = useState("");
+    return (
+        <>
+            <h1>Todo List</h1>
+            <AddTodoForm onAddTodo={setNewTodo} />
+            <p>{newTodo}</p>
+            <TodoList todoList={todoList} />
+        </>
+    );
 }
 
-export default App
+export default App;
